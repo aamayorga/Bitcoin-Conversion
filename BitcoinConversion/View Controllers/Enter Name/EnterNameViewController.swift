fd2754fc3d77369f7f88b176d7f0eed9c247ae2f
aamayorga <amayorga95@outlook.com>
2019-05-05 13:21:59 -0700

    Dismiss keyboard from enter name view controller

--- BitcoinConversion/View Controllers/Enter Name/EnterNameViewController.swift
+++ BitcoinConversion/View Controllers/Enter Name/EnterNameViewController.swift
@@ -17,14 +17,11 @@ class EnterNameViewController: UIViewController {
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     
-    let defaults = UserDefaults.standard
-    
-    var nameTextFieldDelegate = NameTextFieldDelegate()
     var nameEnteredDelegate: EnterNameDelegate!
     var enteredName = String()
     
     override func viewDidLoad() {
-        enterNameTextField.delegate = nameTextFieldDelegate
+        enterNameTextField.delegate = self
     }
     
     @IBAction func doneButton(_ sender: UIButton) {
@@ -32,21 +29,23 @@ class EnterNameViewController: UIViewController {
     }
     
     func saveName() {
-        
         // Check if user entered a name and not an empty string
         enteredName = enterNameTextField.text ?? ""
         guard !enteredName.isEmpty else {
             errorLabel.text = "Please enter valid name"
             return
         }
-
-        defaults.set(enteredName, forKey: "Name")
+        
+        enterNameTextField.resignFirstResponder()
+        errorLabel.text = " "
+        
         nameEnteredDelegate.didEnterName(name: enteredName)
     }
-    
-    func dismissEnterNameViewController() {
-        enterNameTextField.resignFirstResponder()
-        errorLabel.text = ""
-        defaults.set(false, forKey: "firstTimeSetup")
+}
+
+extension EnterNameViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
