b1850b9c17153a5757c73033f3a0a82f1a23a313
aamayorga <amayorga95@outlook.com>
2019-05-05 15:00:36 -0700

    Added file for Segue Constants

---
+++ BitcoinConversion/Helper/SegueConstants.swift
@@ -0,0 +1,16 @@
+//
+//  SegueConstants.swift
+//  BitcoinConversion
+//
+//  Created by Antonio Mayorga on 5/4/19.
+//  Copyright © 2019 Rays Industrial Computers. All rights reserved.
+//
+
+import Foundation
+
+class SegueConstants {
+    static let PopupErrorEmbedSegue                     = "Popup Error Embed Segue"
+    static let EnterNameEmbedSegue                      = "Enter Name Embed Segue"
+    static let CurrencySelectedShowSegue                = "Currency Selected Show Segue"
+    static let CurrencySelectionTableViewEmbedSegue     = "Currency Selection Table View Embed Segue"
+}
